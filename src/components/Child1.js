0902025130d598d060cc5eaf2f20ae5e261856a3
Stephen Ordway <sjo7@njit.edu>
2024-10-14 11:56:15 -0400

    Finished scatter plot. Completed most of bar chart. Need to write more comments on what every line does for future understanding

--- src/components/Child1.js
+++ src/components/Child1.js
@@ -2,23 +2,32 @@ import React, { Component } from "react";
 import * as d3 from "d3";
 
 class Child1 extends Component {
+  // Lifecycle methods and rendering logic
+
   constructor(props) {
     super(props);
     this.state = {};
   }
   componentDidMount() {
-    console.log(this.props.data1);
+    // Called after component is first mounted
+    this.renderChart(); // Initial chart rendering
   }
+
   componentDidUpdate() {
+    // Called when component updates such as after new data is passed as props
+    this.renderChart(); // Re-render chart if component updates
+  }
+
+  renderChart() {
     // Save data to variable
-    var data = this.props.data1;
+    const data = this.props.data1; // Data passed via props
 
     // Set the dimensions and margins of the graph
-    var margin = { top: 10, right: 10, bottom: 30, left: 20 },
+    const margin = { top: 10, right: 10, bottom: 50, left: 30 },
       w = 500 - margin.left - margin.right,
-      h = 300 - margin.top - margin.bottom;
+      h = 400 - margin.top - margin.bottom;
 
-    
+
     // Selecting the child1_svg and set width and height
     var container = d3
       .select(".child1_svg")
@@ -27,11 +36,25 @@ class Child1 extends Component {
       .select(".g_1")
       .attr("transform", `translate(${margin.left},${margin.top})`);
 
+    // Clear any previous content
+    container.selectAll("*").remove();
+
+
+    d3.select(".child1_svg")
+      .append("text") // Append a text element for the title
+      .attr("class", "chart_title") // Add a class for styling
+      .attr("x", w / 2 + margin.left) // Center the title horizontally
+      .attr("y", margin.top + 10) // Position the title relative to the top margin
+      .attr("text-anchor", "middle") // Center the text
+      .style("font-size", "20px") // Set the font size
+      .style("font-weight", "bold") // Make the title bold
+      .text("Total Bill vs Tips"); // The actual title text
+
     // Add X axis
     var x_data = data.map(item => item.total_bill);
     const x_scale = d3
       .scaleLinear()
-      .domain([0, d3.max(x_data)])
+      .domain([0, d3.max(x_data)]) //can use d3.extent(), returns min/max of data
       .range([margin.left, w]);
 
     container
@@ -42,12 +65,21 @@ class Child1 extends Component {
       .attr("transform", `translate(0, ${h})`)
       .call(d3.axisBottom(x_scale));
 
-    // Add Y axis
+    // X-axis label
+    d3.select(".child1_svg")
+      .selectAll(".my_title")
+      .data([0]).join('text')
+      .attr("class", ".my_title")
+      .attr('x', w / 2 + margin.left - 5)
+      .attr("y", h + margin.top + 40) // Adjusts position from bottom
+      .text("Total Bill")
+
+    // Add Y-axis
     var y_data = data.map(item => item.tip);
     const y_scale = d3
       .scaleLinear()
       .domain([0, d3.max(y_data)])
-      .range([h, 0]);
+      .range([h, 0 + 30]);
 
     container
       .selectAll(".y_axis_g")
@@ -56,6 +88,16 @@ class Child1 extends Component {
       .attr("class", "y_axis_g")
       .attr("transform", `translate(${margin.left},0)`)
       .call(d3.axisLeft(y_scale));
+
+    // Y-axis label
+    d3.select(".child1_svg")
+      .append("text")
+      .attr("class", "y_label")
+      .attr("x", -(h / 2) - margin.top)
+      .attr("y", margin.left)
+      .attr("transform", "rotate(-90)")
+      .style("font-size", "20px")
+      .text("Tips");
 
     container
       .selectAll("circle")
@@ -69,6 +111,7 @@ class Child1 extends Component {
       })
       .attr("r", 3)
       .style("fill", "#69b3a2");
+
   }
   render() {
     return (
--- src/components/Child2.js
+++ src/components/Child2.js
@@ -1,15 +1,98 @@
 import React, { Component } from "react";
+import * as d3 from "d3";
 
 class Child2 extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
-  componentDidMount(){
-    console.log(this.props.data2)
+  componentDidMount() {
+    this.renderChart();
   }
+
+  componentDidUpdate() {
+    this.renderChart();
+  }
+
+  renderChart() {
+    // Save data to variable
+    const data = this.props.data2; // Data passed via props
+
+    const rolledData = d3.flatRollup(
+      data,
+      v => d3.mean(v, d => d.tip),
+      d => d.day
+    );
+
+    // Set the dimensions and margins of the graph
+    const margin = { top: 10, right: 10, bottom: 50, left: 30 },
+      w = 500 - margin.left - margin.right,
+      h = 400 - margin.top - margin.bottom;
+
+    // Selecting the child2_svg and set width and height
+    var container = d3
+      .select(".child2_svg")
+      .attr("width", w + margin.left + margin.right)
+      .attr("height", h + margin.top + margin.bottom)
+      .select(".g_2")
+      .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    // Clear any previous content
+    container.selectAll("*").remove();
+
+    // Graph title
+    d3.select(".child2_svg")
+      .append("text")
+      .attr("class", "chart_title")
+      .attr("x", w / 2 + margin.left) // Center the title horizontally
+      .attr("y", margin.top + 10) // Position the title relative to the top margin
+      .attr("text-anchor", "middle") // Center the text
+      .style("font-size", "20px") // Set the font size
+      .text("Average Tip by Day"); // The actual title text
+
+    // X scale
+    const x_scale = d3
+      .scaleBand()
+      .domain(rolledData.map(d => d[0]))
+      .range([0, w])
+      .padding(0.2);
+
+    // Add X axis
+    container
+      .append("g")
+      .attr("class", "x_axis_g")
+      .attr("transform", `translate(0,${h})`)
+      .call(d3.axisBottom(x_scale));
+
+    // Add X-axis label
+
+    // Y scale
+    const y_scale = d3
+      .scaleLinear()
+      .domain([0, d3.max(rolledData, d => d[1])])
+      .range([h, 0 + 30])
+
+    // Y-axis
+    container
+      .append("g")
+      .call(d3.axisLeft(y_scale))
+
+    container
+    .selectAll("mybar")
+    .data(rolledData)
+    .enter()
+    .append("rect")
+    .attr("x", d => x_scale(d[0]))
+    .attr("y", d => y_scale(d[1]))
+    .attr("width", x_scale.bandwidth()) // Set the width of each bar
+    .attr("height", d => h - y_scale(d[1])) // Height of the bar
+    .style("fill", "#69b3a2");
+  }
+
   render() {
-    return <svg className="child2_svg"></svg>;
+    return <svg className="child2_svg">
+      <g className="g_2"></g>
+    </svg>;
   }
 }
 
